45a1ded419ff41fcf331be9dc370be9a690e743f
calvincheng919 <calvin@4mile.io>
2022-09-28 08:49:13 -0700

    v1.0.0

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # looker-marketplace-blocks-test
+
+## Compiling Action
+
+### If you need to make changes to this action:
+- After making changes to index.js file and/or any other asset (for example, tests)
+- run
+```
+ncc build index.js -o dist --source-map --license licenses.txt
+```
+- If `ncc` returns an error, you may need to install `@vercel/ncc` globally by running this first:
+```
+npm install -g @vercel/ncc
+```
---
+++ index.js
@@ -0,0 +1,59 @@
+const core = require('@actions/core');
+const exec = require('@actions/exec');
+// const { exec, execSync ,spawn} = require("child_process");
+require('dotenv').config()
+const LOOKER_BASE_URL = process.env.LOOKER_BASE_URL //process.env.LOOKER_BASE_URL - Persist in GitHub secrets;
+const LOOKER_CLIENT_ID = process.env.LOOKER_CLIENT_ID //process.env.LOOKER_CLIENT_ID; - Persist in GitHub secrets;
+const LOOKER_CLIENT_SECRET = process.env.LOOKER_CLIENT_SECRET //process.env.LOOKER_CLIENT_SECRET; - Persist in GitHub secrets;
+const project = '4_mile_demonstrations'
+const explores = '4_mile_demonstrations/order_items'
+
+// need names of model/view, etc... to feed spectacles
+/* Next Steps
+
+ * 1. Import marketplace.json file
+ * 2. Extract model name
+ * 3. Import manifest.lkml file
+ * 4. Extract project name 
+ * 5. Inject project name into child process that runs spectacles 
+ * 6. Inject model/explore into child process that runs spectacles
+
+*/
+
+async function run() {
+  try {
+    // process.chdir('json-tests')
+    // await exec.exec(`pip install spectacles`)
+    // await exec.exec(`npm test`)
+    
+    await exec.exec(`spectacles lookml \
+      --base-url ${LOOKER_BASE_URL} \
+      --client-id ${LOOKER_CLIENT_ID} \
+      --client-secret ${LOOKER_CLIENT_SECRET} \
+      --project ${project}`
+      )
+    await exec.exec(`spectacles content \
+      --base-url ${LOOKER_BASE_URL} \
+      --client-id ${LOOKER_CLIENT_ID} \
+      --client-secret ${LOOKER_CLIENT_SECRET} \
+      --project ${project}`
+      )
+    await exec.exec(`spectacles sql \
+      --base-url ${LOOKER_BASE_URL} \
+      --client-id ${LOOKER_CLIENT_ID} \
+      --client-secret ${LOOKER_CLIENT_SECRET} \
+      --project ${project} \
+      --explores ${explores}`
+      )
+    await exec.exec(`spectacles assert \
+    --base-url ${LOOKER_BASE_URL} \
+    --client-id ${LOOKER_CLIENT_ID} \
+    --client-secret ${LOOKER_CLIENT_SECRET} \
+    --project ${project}`
+    )
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
